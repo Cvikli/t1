d585115c7b5e1afdb5ab4927e0b8d06f3e5698da
Marcell <havlikmarcell@gmail.com>
2024-10-17 23:27:50 +0200

    Initial commit

--- git.jl
+++ git.jl
@@ -1,12 +1,14 @@
+
+using LibGit2
+using Dates
+
 repo = LibGit2.init(".")
 LibGit2.add!(repo, ".")
 
 LibGit2.commit(repo, "Initial commit")
+
+
 #%%
-using LibGit2
-
-# repo = LibGit2.GitRepo(".")
-
 # Get an iterator for the commit history
 walker = LibGit2.GitRevWalker(repo)
 LibGit2.push_head!(walker)
@@ -14,17 +16,18 @@ LibGit2.push_head!(walker)
 
 # Iterate through the commits
 for oid in walker
-	commit = LibGit2.get(LibGit2.GitCommit, repo, oid)
-	
-	# Get commit information
-	hash = string(oid)
-	author = LibGit2.author(commit)
-	message = LibGit2.message(commit)
-	
-	# Print commit info
-	println("Commit: ", hash[1:7])
-	println("Author: ", author.name, " <", author.email, ">")
-	println("Date:   ", Dates.unix2datetime(author.time))
-	println("    ", message)
-	println()
+    @show oid
+    commit = LibGit2.GitCommit(repo, oid)
+    
+    # Get commit information
+    hash = string(oid)
+    author = LibGit2.author(commit)
+    message = LibGit2.message(commit)
+    
+    # Print commit info
+    println("Commit: ", hash[1:7])
+    println("Author: ", author.name, " <", author.email, ">")
+    println("Date:   ", Dates.unix2datetime(author.time))
+    println("    ", message)
+    println()
 end
